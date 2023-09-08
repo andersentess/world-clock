b61d1da84aec00ea05dcca93fb00034b1361b0b4
andersentess <andersentess@gmail.com>
2023-09-07 17:27:03 -0700

    added dark theme button to be able to toggle between light and dark mode

--- src/index.js
+++ src/index.js
@@ -58,5 +58,17 @@ let citiesSelectElement = document.querySelector("#city");
 
 citiesSelectElement.addEventListener("change", updateCity);
 
+function changeTheme() {
+  let body = document.querySelector("body");
+
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+  } else {
+    body.classList.add("dark");
+  }
+}
+let themeButton = document.querySelector(".theme-button");
+themeButton.addEventListener("click", changeTheme);
+
 updateTime();
 setInterval(updateTime, 1000);
